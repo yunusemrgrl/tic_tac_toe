e4b0f464bc1269836272bf144a95450c59fab64f
Yunus Emre GÜRLÜ <yunusemregurlu@gmail.com>
2022-08-12 01:15:02 +0300

    update readme.md

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import Content from './components/Content';
-import Header from './components/Header';
 
 function App() {
-  return (
-    <>
-      {/* <Header /> */}
-      <Content />
-    </>
-  );
+  return <Content />;
 }
 
 export default App;
