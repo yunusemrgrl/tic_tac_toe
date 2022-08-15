ee199f8fcd005e1c1d129e05a15d53696e4894e1
Yunus Emre GÜRLÜ <yunusemregurlu@gmail.com>
2022-08-15 05:55:31 +0300

    failed commit saved Content, ScoreTable, gameSlice

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useSelector } from 'react-redux';
 import Content from './components/Content';
+import Modal from './components/LoginModal';
+import ScoreBoard from './components/ScoreBoard';
 
 function App() {
-  return <Content />;
+  const isShowModal = useSelector((state) => state.game.isShowModal);
+  return (
+    <div className='flex flex-col '>
+      {!isShowModal && <ScoreBoard />}
+      <Modal />
+      <Content />
+    </div>
+  );
 }
 
 export default App;
